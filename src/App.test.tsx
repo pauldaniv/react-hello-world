41cda0f2360d98f54d76d410271d474e6886d4a8
Pavlo Daniv <paulsegfault@gmail.com>
2022-07-23 14:53:01 +0300

    Fix tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/change 7/i);
+  const linkElement = screen.getByText(/something/i);
   expect(linkElement).toBeInTheDocument();
 });
