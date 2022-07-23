5e292ff8075980eda7a8f2514948a3be000c542a
Pavlo Daniv <paulsegfault@gmail.com>
2022-07-23 03:11:01 +0300

    Generic change

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -2,7 +2,7 @@ import React from "react";
 
 class MyComponent extends React.Component {
   render() {
-    return <h1>Hello, change 2, {this.props.name}</h1>
+    return <h1>Hello, change 3, {this.props.name}</h1>
   }
 }
 
